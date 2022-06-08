17abe504e102094297694854e29dfbc3b4aaedb0
Jonathan Alvarez <jalva100@ucsc.edu>
2022-06-07 20:51:54 -0700

    THINGS DONE IDK

--- src/scenes/levels/chamber2.js
+++ src/scenes/levels/chamber2.js
@@ -515,7 +515,7 @@ class Chamber2 extends Phaser.Scene {
 
 
 
-       this.switch = this.sound.add('switch');
+        this.switch = this.sound.add('switch').setVolume(0.2);
        this.end = this.sound.add('win');
        this.move = this.sound.add('scroll');
        this.select = this.sound.add('select');
--- src/scenes/levels/chamber3.js
+++ src/scenes/levels/chamber3.js
@@ -643,7 +643,7 @@ class Chamber3 extends Phaser.Scene {
 
 
 
-       this.switch = this.sound.add('switch');
+        this.switch = this.sound.add('switch').setVolume(0.2);
        this.end = this.sound.add('win');
        this.move = this.sound.add('scroll');
        this.select = this.sound.add('select');
